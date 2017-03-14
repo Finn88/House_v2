efc0f47b14ad8c1e132477cc9e85681ef0d8efba
Finn88 <mplotnikoff88@gmail.com>
2017-03-14 22:44:47 +0200

    Added project

---
+++ House_v2/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+
+namespace House_v2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contacts()
+        {
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            return View();
+        }
+
+        public ActionResult Price()
+        {
+            return View();
+        }
+
+        public ActionResult Facades()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ House_v2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(House_v2.Startup))]
+namespace House_v2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+          
+        }
+    }
+}
